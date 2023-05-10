960a0cb28d41a6f9e7d01daf1f8581a366aed402
Jouca <progameur009@gmail.com>
2023-05-10 12:00:07 +0200

    Added contructors/methods for Ligne and Station

--- src/Ligne.java
+++ src/Ligne.java
@@ -1,2 +1,55 @@
+import java.util.ArrayList;
+
 public class Ligne {
+    String line_id;
+    String type;
+    String name;
+    ArrayList<Station> stations;
+    ArrayList<Station> terminus;
+
+    Ligne(String line_id, String type, String name, ArrayList<Station> stations, ArrayList<Station> terminus) {
+        this.line_id = line_id;
+        this.type = type;
+        this.name = name;
+        this.stations = stations;
+        this.terminus = terminus;
+    }
+
+    public Boolean contains(Station s) {
+        return this.stations.contains(s);
+    }
+
+    public Boolean isTerminus(Station s) {
+        return this.terminus.contains(s);
+    }
+
+    public String getID() {
+        return this.line_id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public ArrayList<Station> getStations() {
+        return this.stations;
+    }
+
+    public ArrayList<Station> getTerminus() {
+        return this.terminus;
+    }
+
+    public void addStation(Station s) {
+        this.stations.add(s);
+    }
+
+    public void addTerminus(Station s) {
+        if (!this.isTerminus(s)) {
+            this.terminus.add(s);
+        }
+    }
 }
--- src/Station.java
+++ src/Station.java
@@ -1,12 +1,33 @@
 import java.util.ArrayList;
 
-public class Station{
-
+public class Station {
+    String station_id;
     String name;
     ArrayList<Double> coordinates;
-    ArrayList<Ligne> lignes;
 
-    public Boolean contains(Ligne l){
-        return lignes.contains(l);
+    Station(String station_id, String name, ArrayList<Double> coordinates) {
+        this.station_id = station_id;
+        this.name = name;
+        this.coordinates = coordinates;
+    }
+
+    public String getID() {
+        return station_id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public ArrayList<Double> getCoordinates() {
+        return this.coordinates;
+    }
+
+    public Double getLatitude() {
+        return this.coordinates.get(0);
+    }
+
+    public Double getLongitude() {
+        return this.coordinates.get(1);
     }
 }
