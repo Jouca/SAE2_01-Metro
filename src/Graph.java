06a202e185144f8fe1111a3d9ae54e8e079983e5
Jouca <progameur009@gmail.com>
2023-05-11 11:51:06 +0200

    Fix files

--- src/Graph.java
+++ src/Graph.java
@@ -1,7 +1,55 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class Graph {
-    ArrayList<Station> stations;
-    ArrayList<Edge> edges;
+    HashSet<Station> stations;
+    HashMap<Station, Edge> anterior;
+
+    public void addstation(Station station) {
+        stations.add(station);
+    }
+
+    public ArrayList<Edge> Dijkstra(Station begin, Station end){
+        begin.setDistance(0);
+        HashSet<Station> nodes = new HashSet<>(stations);
+        while(!nodes.isEmpty()) {
+            Station s1 = minneighbor(nodes);
+            for(Edge e : s1.getNeighbor()){
+                update_weight(s1, e.getotherParent(s1));
+            }
+        }
+
+
+
+        ArrayList<Edge> a = new ArrayList<>();
+        while(end != begin) {
+            a.add(0, anterior.get(end));
+            end = anterior.get(end).getotherParent(end);
+        }
+
+        return a;
+    }
+
+    private Station minneighbor(HashSet<Station> set) {
+        Integer mini = Integer.MAX_VALUE;
+        Station sommet = null ;
+        for(Station s: set) {
+            for(Edge i : s.getNeighbor()){
+                if(i.getWeight() < mini){
+                    mini = i.getWeight();
+                    sommet = i.getotherParent(s);
+                }
+            }
+        }
+        return sommet;
+    }
+
+    private void update_weight(Station s1, Station s2) {
+        if(s2.getDistance() > (s1.getDistance() + s1.getminiedge(s2).getWeight())){
+            s2.setDistance((s1.getDistance() + s1.getminiedge(s2).getWeight()));
+            anterior.put(s2, s1.getminiedge(s2));
+        }
+    }
 
 }
--- src/Station.java
+++ src/Station.java
@@ -4,11 +4,44 @@ public class Station {
     String station_id;
     String name;
     ArrayList<Double> coordinates;
+    ArrayList<Ligne> lignes;
+
+    //Distance and neighbor it's use for Disjkra
+    Integer distance = Integer.MAX_VALUE;
+    ArrayList<Edge> neighbor = new ArrayList<Edge>();
 
     Station(String station_id, String name, ArrayList<Double> coordinates) {
         this.station_id = station_id;
         this.name = name;
         this.coordinates = coordinates;
+    }
+
+    public Boolean contains(Ligne l){
+        return lignes.contains(l);
+    }
+
+    public ArrayList<Edge> getNeighbor() {
+        return neighbor;
+    }
+
+    public Integer getDistance() {
+        return distance;
+    }
+
+    public void setDistance(Integer distance) {
+        this.distance = distance;
+    }
+
+    public Edge getminiedge(Station s) {
+        Integer mini = Integer.MAX_VALUE;
+        Edge temp = null;
+        for(Edge i : neighbor){
+            if(i.containStation(this) && i.containStation(s) && i.getWeight() < mini) {
+                mini = i.getWeight();
+                temp = i;
+            }
+        }
+        return temp;
     }
 
     public String getID() {
