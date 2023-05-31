d5f3f7f2efa2c6306a4a2729b48b234242748898
L3ChatNoir <108089371+L3ChatNoir@users.noreply.github.com>
2023-05-31 11:55:12 +0200

    find best path with steps

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 import com.opencsv.exceptions.CsvValidationException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException, CsvValidationException {
@@ -8,5 +10,7 @@ public class Main {
 
         StationFinder stationFinder = new StationFinder();
         stationFinder.findBestPath("Ch\u00e2telet", "Villejuif L\u00e9o Lagrange");
+
+        stationFinder.printfindBestPath(new ArrayList<String>(Arrays.asList( "Ch\u00e2telet", "Villejuif Louis Aragon", "Gare de l'Est")));
     }
 }
--- src/main/java/StationFinder.java
+++ src/main/java/StationFinder.java
@@ -57,4 +57,50 @@ public class StationFinder {
         }
         return stations;
     }
+
+    public ArrayList<Station> findBestPath(ArrayList<String> stations){
+        ArrayList<Station> path = new ArrayList<Station>();
+
+        for(int i=0; i < stations.size()-1; i++){
+            ArrayList<Station> beginStations =  findStationsByName(stations.get(i));
+            ArrayList<Station> arrivalStations = findStationsByName(stations.get(i+1));
+
+            if (beginStations.size() == 0 || arrivalStations.size() == 0) {
+                return null;
+            }
+    
+            Station minDijkstraStationArrival = null;
+            Dijkstra minDijkstraPath = null;
+            int minDijkstraTime = Integer.MAX_VALUE;
+    
+            for (Station beginStation : beginStations) {
+                Dijkstra dijkstraStations = new Dijkstra(graphe, beginStation);
+                for (Station arrivalStation : arrivalStations) {
+                    if (dijkstraStations.timeTo(arrivalStation.getID()) < minDijkstraTime) {
+                        minDijkstraTime = dijkstraStations.timeTo(arrivalStation.getID());
+                        minDijkstraStationArrival = arrivalStation;
+                        minDijkstraPath = dijkstraStations;
+                    }
+                }
+            }
+            for(Station s : minDijkstraPath.shortestPathTo(minDijkstraStationArrival, graphe)){
+                path.add(s);
+            }
+        }
+
+        return path;
+    }
+
+    public void printfindBestPath(ArrayList<String> stations){
+        ArrayList<Station> path = this.findBestPath(stations);
+
+        String etape = "";
+        for(int i =1; i<stations.size() -1; i++){
+            etape+= stations.get(i);
+        }
+        System.out.println("\n========= "+ stations.get(0) + " vers " + stations.get(stations.size() -1) + " en passant par " + etape + " =========");
+        for(Station s : path){
+            System.out.print(" ==> " + s.getName() + " (ligne " + s.getLigne().getName() + ")\n");
+        }
+    }
 }
